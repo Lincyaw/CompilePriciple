819a133870da83f6d4425dbf71bbb63edd67254e
lincyaw <814750204@qq.com>
2020-11-26 12:24:36 +0800

    实验报告完成

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 
 int main() {
 //    testModules();
-    cout<<"词法分析开始"<<endl;
+    cout<<"Lexical analysis begin"<<endl;
     Lexical_analyzer lex;
     lex.lexical();
-    cout<<"词法分析结束,输出symbol.txt,token.txt"<<endl;
+    cout<<"Lexical analysis ends. Output: symbol.txt,token.txt "<<endl;
     Grammar_Analyzer gra;
 //    dbg(lex.sysTable);
-    cout<<"语法分析开始"<<endl;
+    cout<<"Grammar analysis begin"<<endl;
     gra.LR1(lex.sysTable);
-    cout<<"语法分析结束,输出producer.txt"<<endl;
+    cout<<"Grammar analysis ends. Output: producer.txt "<<endl;
     return 0;
 }
